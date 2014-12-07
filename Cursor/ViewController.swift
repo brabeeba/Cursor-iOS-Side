3e56be6d177497c14506532b7fb90cd168324ba8
brabeeba <brabeeba@gmail.com>
2014-12-07 04:58:29 -0500

    Initial Commit

---
+++ Cursor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cursor
+//
+//  Created by Brabeeba Wang on 12/7/14.
+//  Copyright (c) 2014 Brabeeba Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
